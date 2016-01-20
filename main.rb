548390be0e37bcc88856fb61c2142c009fea6602
Sarah Sunday <sarahwork@SarahSundays-MacBook-Pro.local>
2016-01-20 14:14:37 -0600

    Added pages, working on initial settings

--- main.rb
+++ main.rb
@@ -1,21 +1,35 @@
 require 'sinatra'
-require 'haml'
+require 'data_mapper'
 require_relative 'tic_tac_toe_board.rb'
+require_relative 'tic_tac_toe_rules.rb'
+DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/tictactoe.db")
+
+class Game
+  include DataMapper::Resource
+  property :player_one_marker, String
+	property :player_two_marker, String
+  property :rules, TicTacToeRules
+end
+DataMapper.finalize
+Game.auto_upgrade!
 
 get '/' do
 	@title = 'Home'
 	erb :index
 end
 
-get '/start_up' do
-  @title = "Start Up"
-  erb :start_up
+get '/settings' do
+  @title = "Game Settings"
+  erb :settings
 end
 
-post '/start_up' do
-  "You said '#{params[:message]}'"
+post '/settings' do
+	@title = "Game Settings"
+	@rules = TicTacToeRules.new(TicTacToeBoard.new, player_one_marker, player_two_marker)
+  erb :settings_end
 end
 
 not_found do
+	@title = "Not found!"
   erb :not_found
 end
