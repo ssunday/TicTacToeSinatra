ac15cabefc32ea0166962daf1a1f5365e7b213f7
Sarah Sunday <sarahwork@SarahSundays-MacBook-Pro.local>
2016-01-21 09:46:50 -0600

    Added options and tweaked layout
    Removed data mapper

--- main.rb
+++ main.rb
@@ -1,21 +1,21 @@
 require 'sinatra'
-require 'data_mapper'
-require 'dm-types'
+# require 'data_mapper'
+# require 'dm-types'
+# require "dm-core"
 require_relative 'tic_tac_toe_board.rb'
 require_relative 'tic_tac_toe_rules.rb'
-DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite3://#{Dir.pwd}/game.db")
-
-class Game
-  include DataMapper::Resource
-
-  property :player_one_marker, 	String, :key => true
-  property :player_two_marker, 	String, :key => true
-  #property :rules, Object, :key => true
-
-end
-
-DataMapper.finalize.auto_upgrade!
-
+# DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite3://#{Dir.pwd}/game.db")
+#
+# class Game
+#   include DataMapper::Resource
+#
+#   property :player_one_marker, 	String, :key => true
+#   property :player_two_marker, 	String, :key => true
+#   property :rules, Object, :key => true, :required => false
+#
+# end
+#
+# DataMapper.finalize.auto_migrate!
 get '/' do
 	@title = 'Home'
 	erb :index
@@ -28,18 +28,24 @@ end
 
 post '/settings' do
 	@title = "Game Settings"
-	#Game.create(player_one_marker: params[:player_one_marker], player_two_marker: params[:player_two_marker], rules: TicTacToeRules.new(TicTacToeBoard.new, first_player: params[:player_one_marker] , player_one_marker: params[:player_one_marker], player_two_marker: params[:player_two_marker]))
+  $player_one_marker = params[:player_one_marker]
+	$game = TicTacToeRules.new(TicTacToeBoard.new, first_player: params[:player_one_marker] , player_one_marker: params[:player_one_marker], player_two_marker: params[:player_two_marker])
   erb :settings_done
 end
 
 get '/play_game' do
 	@title = "Game"
+  #$player_one_marker
 	erb :play_game
 end
 
 get '/end_game' do
 	@title = "Game Over"
 	erb :end_game
+end
+
+put "/end_game" do
+  redirect '/'
 end
 
 not_found do
