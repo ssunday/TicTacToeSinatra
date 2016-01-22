cc7b3c59acad70a57e1784a7de17673e39f3045b
Sarah Sunday <sarahwork@SarahSundays-MacBook-Pro.local>
2016-01-22 14:27:23 -0600

    Added Error handling and made board easier to read

--- main.rb
+++ main.rb
@@ -2,6 +2,8 @@ require 'sinatra'
 require_relative 'tic_tac_toe_board.rb'
 require_relative 'tic_tac_toe_rules.rb'
 require_relative 'tic_tac_toe_ai.rb'
+require 'sinatra/formkeeper'
+
 use Rack::Session::Cookie, :key => 'rack.session', :path => '/', :secret => 'tic-tac-toe'
 
 get '/' do
@@ -17,27 +19,37 @@ end
 
 post '/settings' do
 	@title = "Game Settings"
-	session["player_one_marker"] = params[:player_one_marker]
-	session["player_two_marker"] = params[:player_two_marker]
-	if params[:first_player].eql?("player_one_marker")
-		first_player = params[:player_one_marker]
-	else
-		first_player = params[:player_two_marker]
+	form do
+		field :player_one_marker, :present => true, :length => 1
+		field :player_two_marker, :present => true, :length => 1
 	end
-	if params[:player_one_type].eql?("AI")
-		session["player_one_ai"] = TicTacToeAi.new(ai_marker: session["player_one_marker"], other_player_marker: session["player_two_marker"])
-	else
-		session["player_one_ai"] = nil
-	end
-	if params[:player_two_type].eql?("AI")
-		session["player_two_ai"] = TicTacToeAi.new(ai_marker: session["player_two_marker"], other_player_marker: session["player_one_marker"])
-	else
-		session["player_two_ai"] = nil
-	end
-	session["game"] = TicTacToeRules.new(TicTacToeBoard.new, first_player: first_player , player_one: session["player_one_marker"], player_two: session["player_two_marker"])
-	session["board"] = session["game"].get_board
-	session["board_array"] = session["game"].get_array_board
-	redirect '/play_game'
+
+	if form.failed?
+    erb :settings
+  else
+		session["player_one_marker"] = params[:player_one_marker]
+		session["player_two_marker"] = params[:player_two_marker]
+		if params[:first_player].eql?("player_one_marker")
+			first_player = params[:player_one_marker]
+		else
+			first_player = params[:player_two_marker]
+		end
+
+		if params[:player_one_type].eql?("AI")
+			session["player_one_ai"] = TicTacToeAi.new(ai_marker: session["player_one_marker"], other_player_marker: session["player_two_marker"])
+		else
+			session["player_one_ai"] = nil
+		end
+		if params[:player_two_type].eql?("AI")
+			session["player_two_ai"] = TicTacToeAi.new(ai_marker: session["player_two_marker"], other_player_marker: session["player_one_marker"])
+		else
+			session["player_two_ai"] = nil
+		end
+		session["game"] = TicTacToeRules.new(TicTacToeBoard.new, first_player: first_player , player_one: session["player_one_marker"], player_two: session["player_two_marker"])
+		session["board"] = session["game"].get_board
+		session["board_array"] = session["game"].get_array_board
+		redirect '/play_game'
+  end
 end
 
 get '/play_game' do
