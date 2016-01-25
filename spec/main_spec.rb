559cda93881cd755923f9bde4a5707d57d721644
Sarah Sunday <sarahwork@SarahSundays-MacBook-Pro.local>
2016-01-25 13:38:24 -0600

    Added initial rspec

---
+++ spec/main_spec.rb
@@ -0,0 +1,20 @@
+require 'rack/test'
+require 'rspec'
+
+ENV['RACK_ENV'] = 'test'
+
+require File.expand_path '../../main.rb', __FILE__
+
+module RSpecMixin
+  include Rack::Test::Methods
+  def app() Sinatra::Application end
+end
+
+RSpec.configure { |c| c.include RSpecMixin }
+
+describe "Tic Tac Toe Web App" do
+  it "should allow accessing the home page" do
+    get '/'
+    expect(last_response).to be_ok
+  end
+end
