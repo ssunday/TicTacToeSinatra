9b0a9ed43b29dc7d7f22be01191b92e63e95cb13
Sarah Sunday <sarahwork@SarahSundays-MacBook-Pro.local>
2016-01-26 11:16:28 -0600

    Actually kind of works now.
    AI is now vaguely functioning.

--- tictactoe/tic_tac_toe_ai.rb
+++ tictactoe/tic_tac_toe_ai.rb
@@ -16,7 +16,7 @@ class TicTacToeAi
 
     available_spaces.each do |space|
       next_player = switch_turn(current_player)
-      board.set_board_location(space.to_i, current_player)
+      board.set_board_location(space.to_i, next_player)
       best_score[space.to_i] = move(board, next_player, depth + 1, {})
       board.set_board_location(space.to_i, space)
     end
