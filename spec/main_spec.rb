76b6f9c8576062e53bff6034b817bc9244eb7a2f
Sarah Sunday <sarahwork@SarahSundays-MacBook-Pro.local>
2016-02-03 14:43:09 -0600

    Refactored test code

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -1,9 +1,10 @@
 require 'rack/test'
 require 'rspec'
+require File.expand_path '../../main.rb', __FILE__
+require File.expand_path '../../lib/game.rb', __FILE__
 
 ENV['RACK_ENV'] = 'test'
 
-require File.expand_path '../../main.rb', __FILE__
 
 module RSpecMixin
   include Rack::Test::Methods
@@ -71,57 +72,84 @@ describe "Tic Tac Toe Web App" do
 
   describe "Play game" do
 
-    before(:each) do
-      post '/settings', :player_one_marker => @player_one_marker, :player_two_marker => @player_two_marker, :player_one_type => "Human", :player_two_type => "Human", :first_player => "player_one_marker"
+    def set_up_new_game(game)
+      game.player_one_marker = @player_one_marker
+      game.player_two_marker = @player_two_marker
+      game.player_one_ai = false
+      game.player_two_ai = false
+      game.player_turn = @player_one_marker
+      game.game_board = ["0", "1", "2", "3", "4", "5", "6", "7", "8"]
+      game.save
+      game
     end
 
     it "should be ok" do
-      get '/play_game', :game_id => 3
+      game = Game.new
+      game = set_up_new_game(game)
+  		game.save
+      get '/play_game', :game_id => game.id
       expect(last_response).to be_ok
     end
 
     it "should post with player input" do
-      follow_redirect!
-      post '/play_game', :game_id => 4, :spot => "0"
+      game = Game.new
+  		game = set_up_new_game(game)
+  		game.save
+      post '/play_game', :game_id => game.id, :spot => "0"
       expect(last_response).to be_ok
     end
 
     it "should post with player input and switch to next turn" do
-      follow_redirect!
-      post '/play_game', :game_id => 5, :spot => "0"
-      get '/play_game', :game_id => 5
+      game = Game.new
+      game = set_up_new_game(game)
+  		game.save
+      post '/play_game', :game_id => game.id, :spot => "0"
+      game_rules = create_new_game_rules(game)
+      game = game_turn(game, game_rules, "0")
+      game.save
+      get '/play_game', :game_id => game.id
       expect(last_response.body).to include("Marker: #{@player_two_marker}")
     end
 
     it "should post with player input and show marked location" do
-      follow_redirect!
-      post '/play_game', :game_id => 6, :spot => "0"
-      get '/play_game', :game_id => 6
+      game = Game.new
+  		game = set_up_new_game(game)
+  		game.save
+      post '/play_game', :game_id => game.id, :spot => "0"
+      game_rules = create_new_game_rules(game)
+      game = game_turn(game, game_rules, "0")
+      game.save
+      get '/play_game', :game_id => game.id
+      expect(last_response.body).to include("Marker: #{@player_two_marker}")
       expect(last_response.body).to include(@player_one_marker)
     end
 
     it "should go to end game and show won when game has been won" do
-      follow_redirect!
-      post '/play_game', :game_id => 7, :spot => "0"
-      post '/play_game', :game_id => 7, :spot => "1"
-      post '/play_game', :game_id => 7, :spot => "3"
-      post '/play_game', :game_id => 7, :spot => "4"
-      post '/play_game', :game_id => 7, :spot => "6"
-      post '/play_game', :game_id => 7, :spot => "7"
+      game = Game.new
+      game = set_up_new_game(game)
+  		game.save
+      post '/play_game', :game_id => game.id, :spot => "0"
+      post '/play_game', :game_id => game.id, :spot => "1"
+      post '/play_game', :game_id => game.id, :spot => "3"
+      post '/play_game', :game_id => game.id, :spot => "4"
+      post '/play_game', :game_id => game.id, :spot => "6"
+      post '/play_game', :game_id => game.id, :spot => "7"
       expect(last_response.body).to include("Won")
     end
 
     it "should go to end game and show tied when game has been tied" do
-      follow_redirect!
-      post '/play_game', :game_id => 8, :spot => "0"
-      post '/play_game', :game_id => 8, :spot => "1"
-      post '/play_game', :game_id => 8, :spot => "2"
-      post '/play_game', :game_id => 8, :spot => "3"
-      post '/play_game', :game_id => 8, :spot => "4"
-      post '/play_game', :game_id => 8, :spot => "6"
-      post '/play_game', :game_id => 8, :spot => "5"
-      post '/play_game', :game_id => 8, :spot => "8"
-      post '/play_game', :game_id => 8, :spot => "7"
+      game = Game.new
+      game = set_up_new_game(game)
+  		game.save
+      post '/play_game', :game_id => game.id, :spot => "0"
+      post '/play_game', :game_id => game.id, :spot => "1"
+      post '/play_game', :game_id => game.id, :spot => "2"
+      post '/play_game', :game_id => game.id, :spot => "3"
+      post '/play_game', :game_id => game.id, :spot => "4"
+      post '/play_game', :game_id => game.id, :spot => "6"
+      post '/play_game', :game_id => game.id, :spot => "5"
+      post '/play_game', :game_id => game.id, :spot => "8"
+      post '/play_game', :game_id => game.id, :spot => "7"
       expect(last_response.body).to include("Tied")
     end
   end
@@ -130,7 +158,7 @@ describe "Tic Tac Toe Web App" do
 
     it "can see previous games" do
       get '/previous_games'
-      expect(last_response.body).to include("#{@player_two_marker}")
+      expect(last_response.body).to include("#{@player_one_marker}")
     end
 
   end
